6e4a72da6ad8f7382df6bf538a7699c9401f236a
Shay Masuda <Shaymasuda@gmail.com>
2019-02-04 10:29:21 +0200

    Space Defense init

---
+++ Unity3d/Space Defense/Assets/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform target;
+    private int waypointIndex = 0;
+
+    private void Start()
+    {
+        target = Waypoints.points[waypointIndex];
+    }
+
+    private void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if(Vector3.Distance(transform.position, target.position) <= 0.3f )
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    private void GetNextWaypoint()
+    {
+        if (waypointIndex >= Waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        waypointIndex++;
+        target = Waypoints.points[waypointIndex];
+    }
+}
