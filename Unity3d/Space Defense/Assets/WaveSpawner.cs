ade36457e53bf52ba9c45b8b236c98cd31ff5113
Shay Masuda <Shaymasuda@gmail.com>
2019-02-04 11:58:22 +0200

    WaveSpawner

---
+++ Unity3d/Space Defense/Assets/WaveSpawner.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+    public float timeBetweenWaves = 5f;
+    public Text waveCountdownText;
+
+    private float countdown = 2f;
+    private int waveNumber = 0;
+
+    private void Update()
+    {
+        if (countdown <= 0)
+        {
+            StartCoroutine( SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+
+        waveCountdownText.text = Mathf.Floor(countdown).ToString();
+
+        countdown -= Time.deltaTime;
+    }
+
+    IEnumerator SpawnWave()
+    {
+        Debug.Log("Wave incoming");
+        waveNumber++;
+        for (int i = 0; i < waveNumber; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
